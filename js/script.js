608ced2f9f22074771a30e8852f8c651c9e13073
erfangolabdar79 <erfangolabdar.work@gmail.com>
2025-10-31 20:24:31 +0330

    Add elegant login page for باغچه پونه

--- js/script.js
+++ js/script.js
@@ -1,66 +1,66 @@
 // داده‌های منو از Menu.txt
 const menus = {
   breakfast: [
-    { name: "نیمرو", desc: "تازه و داغ", img: "images/nimroo.jpg",price: "40,000 تومان" },
-    { name: "املت سنتی", desc: "با گوجه تازه", img: "images/omlet.jpg",price: "40,000 تومان"  },
-    { name: "املت سوسیس", desc: "املت با سوسیس", img: "images/omlet-sosis.jpg",price: "40,000 تومان"  },
-    { name: "سوسیس تخم مرغ", desc: "سوسیس و تخم مرغ", img: "images/sosis-egg.jpg" ,price: "40,000 تومان" },
-    { name: "سوسیس تخم مرغ ویژه", desc: "همراه با سیب زمینی و قارچ", img: "images/sosis-egg-special.jpg",price: "40,000 تومان"  },
-    { name: "صبحانه سنتی", desc: "پنیر، گردو، خیار و گوجه", img: "images/traditional-breakfast.jpg",price: "40,000 تومان"  }
+    { name: "نیمرو", desc: "تازه و داغ", img: "images/nimroo.jpg",price: "70,000 تومان" },
+    { name: "املت سنتی", desc: "با گوجه تازه", img: "images/omlet.jpg",price: "150,000 تومان"  },
+    { name: "املت سوسیس", desc: "املت با سوسیس", img: "images/omlet-sosis.jpg",price: "190,000 تومان"  },
+    { name: "سوسیس تخم مرغ", desc: "سوسیس و تخم مرغ", img: "images/sosis-egg.jpg" ,price: "150,000 تومان" },
+    { name: "سوسیس تخم مرغ ویژه", desc: "همراه با سیب زمینی و قارچ", img: "images/sosis-egg-special.jpg",price: "280,000 تومان"  },
+    { name: "صبحانه سنتی", desc: "پنیر، گردو، خیار و گوجه", img: "images/traditional-breakfast.jpg"}
   ],
   main: [
-    { name: "جگر", desc: "جگر تازه گوسفندی", img: "images/jigar.jpg",price: "40,000 تومان" },
-    { name: "دل", desc: "دل تازه گوسفندی ", img: "images/del.jpg",price: "40,000 تومان" },
-    { name: "قلوه", desc: "قلوه تازه گوسفندی", img: "images/gholveh.jpg",price: "40,000 تومان" },
-    { name: "جگرپیچ", desc: "جگرپیچ مخصوص", img: "images/jigar-pich.jpg",price: "40,000 تومان" },
-    { name: "دنبه", desc: "دنبه تازه کبابی", img: "images/donbeh.jpg",price: "40,000 تومان" },
-    { name: "خوئک", desc: " خوئک تازه کبابی", img: "images/khoeyk.jpg",price: "40,000 تومان" },
-    { name: "خوش گوشت", desc: "    ", img: "images/khoshgoosht.jpg",price: "40,000 تومان" },
-    { name: "جوجه بدون استخوان", desc: "جوجه کباب بدون استخوان", img: "images/joje.jpg",price: "40,000 تومان" },
-    { name: "چنجه", desc: "گوشت تازه و دنبه", img: "images/chenje.jpg",price: "40,000 تومان" },
-    { name: "بال کبابی (5 تکه)", desc: "بال مرغ کبابی", img: "images/wing.jpg",price: "40,000 تومان" },
-    { name: "بازو کبابی (4 تکه)", desc: "بازوی مرغ کبابی", img: "images/bazu.jpg",price: "40,000 تومان" },
-    { name: "پنیر کبابی", desc: "پنیر مخصوص کبابی", img: "images/paneer.jpg",price: "40,000 تومان" },
-    { name: "قارچ کبابی", desc: "قارچ تازه کبابی", img: "images/mushroom.jpg",price: "40,000 تومان" },
-    { name: "سوسیس شکاری", desc: "سوسیس شکاری کبابی", img: "images/sausage.jpg",price: "40,000 تومان" },
-    { name: "سوسیس پنیری", desc: "سوسیس پنیری کبابی", img: "images/cheese-sausage.jpg",price: "40,000 تومان" },
-    { name: "سیب زمینی سرخ شده", desc: "سیب زمینی تازه و ترد", img: "images/fries.jpg",price: "40,000 تومان" },
-    { name: "میرزاقاسمی", desc: "غذای سنتی شمالی", img: "images/mirza.jpg",price: "40,000 تومان" }
+    { name: "جگر", desc: "جگر تازه گوسفندی", img: "images/jigar.jpg",price: "57,000 تومان" },
+    { name: "دل", desc: "دل تازه گوسفندی ", img: "images/del.jpg",price: "57,000 تومان" },
+    { name: "قلوه", desc: "قلوه تازه گوسفندی", img: "images/gholveh.jpg",price: "57,000 تومان" },
+    { name: "جگرپیچ", desc: "جگرپیچ مخصوص", img: "images/jigar-pich.jpg",price: "90,000 تومان" },
+    { name: "دنبه", desc: "دنبه تازه کبابی", img: "images/donbeh.jpg",price: "57,000 تومان" },
+    { name: "خوئک", desc: " خوئک تازه کبابی", img: "images/khoeyk.jpg",price: "57,000 تومان" },
+    { name: "خوش گوشت", desc: "    ", img: "images/khoshgoosht.jpg",price: "57,000 تومان" },
+    { name: "جوجه بدون استخوان", desc: "جوجه کباب بدون استخوان", img: "images/joje.jpg",price: "250,000 تومان" },
+    { name: "چنجه", desc: "گوشت تازه و دنبه", img: "images/chenje.jpg",price: "350,000 تومان" },
+    { name: "بال کبابی (5 تکه)", desc: "بال مرغ کبابی", img: "images/wing.jpg",price: "160,000 تومان" },
+    { name: "بازو کبابی (4 تکه)", desc: "بازوی مرغ کبابی", img: "images/bazu.jpg",price: "160,000 تومان" },
+    { name: "پنیر کبابی", desc: "پنیر مخصوص کبابی", img: "images/paneer.jpg",price: "80,000 تومان" },
+    { name: "قارچ کبابی", desc: "قارچ تازه کبابی", img: "images/mushroom.jpg",price: "60,000 تومان" },
+    { name: "سوسیس شکاری", desc: "سوسیس شکاری کبابی", img: "images/sausage.jpg",price: "100,000 تومان" },
+    { name: "سوسیس پنیری", desc: "سوسیس پنیری کبابی", img: "images/cheese-sausage.jpg",price: "70,000 تومان" },
+    { name: "سیب زمینی سرخ شده", desc: "سیب زمینی تازه و ترد", img: "images/fries.jpg",price: "170,000 تومان" },
+    { name: "میرزاقاسمی", desc: "غذای سنتی شمالی", img: "images/mirza.jpg",price: "170,000 تومان" }
   ],
   snacks: [
-    { name: "گوجه کبابی", desc: "گوجه تازه", img: "images/tomato.jpg",price: "40,000 تومان" },
-    { name: "ماست موسیر", desc: "ماست موسیر محلی", img: "images/yogurt.jpg",price: "40,000 تومان" },
+    { name: "گوجه کبابی", desc: "گوجه تازه", img: "images/tomato.jpg" },
+    { name: "ماست موسیر", desc: "ماست موسیر محلی", img: "images/yogurt.jpg" },
     { name: "زیتون", desc: "زیتون تازه", img: "images/olive.jpg" },
-    { name: "زیتون پرورده", desc: "زیتون پرورده شمالی", img: "images/olive-mix.jpg",price: "40,000 تومان" },
-    { name: "ترشی", desc: "ترشی خانگی", img: "images/torshi.jpg",price: "40,000 تومان" },
-    { name: "چیپس", desc: "چیپس بسته‌ای", img: "images/chips.jpg",price: "40,000 تومان" },
-    { name: "تخمه", desc: " ", img: "images/seeds.jpg",price: "40,000 تومان" },
-    { name: "بادام زمینی", desc: "بادام زمینی بو داده", img: "images/peanut.jpg",price: "40,000 تومان" },
-    { name: "بستنی", desc: "سنتی و میوه‌ای", img: "images/icecream.jpg",price: "40,000 تومان" }
+    { name: "زیتون پرورده", desc: "زیتون پرورده شمالی", img: "images/olive-mix.jpg" },
+    { name: "ترشی", desc: "ترشی خانگی", img: "images/torshi.jpg" },
+    { name: "چیپس", desc: "چیپس بسته‌ای", img: "images/chips.jpg" },
+    { name: "تخمه", desc: " ", img: "images/seeds.jpg" },
+    { name: "بادام زمینی", desc: "بادام زمینی بو داده", img: "images/peanut.jpg" },
+    { name: "بستنی", desc: "سنتی و میوه‌ای", img: "images/icecream.jpg" }
   ],
   drinks: [
-    { name: "نوشابه", desc: "کوکا، کوکا زیرو، فانتا، لیموناد، اسپرایت", img: "images/soda.jpg",price: "40,000 تومان" },
-    { name: "دوغ", desc: "آبعلی و سنتی", img: "images/doogh.jpg",price: "40,000 تومان" },
-    { name: "دلستر", desc: "استوایی و لیمویی", img: "images/delster.jpg",price: "40,000 تومان" },
-    { name: "آب معدنی", desc: " ", img: "images/water.jpg",price: "40,000 تومان" }
+    { name: "نوشابه", desc: "کوکا، کوکا زیرو، فانتا، لیموناد، اسپرایت", img: "images/soda.jpg" },
+    { name: "دوغ", desc: "آبعلی و سنتی", img: "images/doogh.jpg" },
+    { name: "دلستر", desc: "استوایی و لیمویی", img: "images/delster.jpg" },
+    { name: "آب معدنی", desc: " ", img: "images/water.jpg" }
   ],
   hot: [
-    { name: "سرویس چای", desc: "چای ایرانی", img: "images/tea.jpg",price: "40,000 تومان" },
-    { name: "چای دارچین", desc: "چای معطر دارچین", img: "images/cinnamon-tea.jpg",price: "40,000 تومان" },
-    { name: "چای زعفران", desc: "چای با زعفران", img: "images/saffron-tea.jpg",price: "40,000 تومان" },
-    { name: "چای گل محمدی", desc: "چای با گل محمدی", img: "images/rose-tea.jpg",price: "40,000 تومان" },
-    { name: "چای بهار نارنج", desc: "چای معطر بهارنارنج", img: "images/orange-tea.jpg",price: "40,000 تومان" },
-    { name: "اسپرسو", desc: "قهوه اسپرسو", img: "images/espresso.jpg",price: "40,000 تومان" },
-    { name: "نسکافه", desc: "قهوه فوری", img: "images/nescafe.jpg",price: "40,000 تومان" }
+    { name: "سرویس چای", desc: "چای ایرانی", img: "images/tea.jpg" },
+    { name: "چای دارچین", desc: "چای معطر دارچین", img: "images/cinnamon-tea.jpg" },
+    { name: "چای زعفران", desc: "چای با زعفران", img: "images/saffron-tea.jpg" },
+    { name: "چای گل محمدی", desc: "چای با گل محمدی", img: "images/rose-tea.jpg" },
+    { name: "چای بهار نارنج", desc: "چای معطر بهارنارنج", img: "images/orange-tea.jpg" },
+    { name: "اسپرسو", desc: "قهوه اسپرسو", img: "images/espresso.jpg" },
+    { name: "نسکافه", desc: "قهوه فوری", img: "images/nescafe.jpg" }
   ],
   cold: [
-    { name: "آب طالبی", desc: "خنک و طبیعی", img: "images/melon.png",price: "40,000 تومان" },
-    { name: "آب هندوانه", desc: "آب هندوانه تازه", img: "images/watermelon.png",price: "40,000 تومان" },
-    { name: "موهیتو طبیعی", desc: "موهیتوی خنک", img: "images/mojito.jpg",price: "40,000 تومان" },
-    { name: "هندوانه", desc: "برش هندوانه تازه", img: "images/watermelon-slice.png",price: "40,000 تومان" }
+    { name: "آب طالبی", desc: "خنک و طبیعی", img: "images/melon.png" },
+    { name: "آب هندوانه", desc: "آب هندوانه تازه", img: "images/watermelon.png" },
+    { name: "موهیتو طبیعی", desc: "موهیتوی خنک", img: "images/mojito.jpg" },
+    { name: "هندوانه", desc: "برش هندوانه تازه", img: "images/watermelon-slice.png" }
   ],
   hookah: [
-    { name: "پرتقال خامه", desc: "طعم پرتقال و خامه", img: "images/yek.png",price: "40,000 تومان" },
+    { name: "پرتقال خامه", desc: "طعم پرتقال و خامه", img: "images/yek.png",price: "250,000 تومان" },
     { name: "پرتقال نعنا", desc: "پرتقال با نعنا", img: "images/hookah2.png" },
     { name: "لیمو نعنا", desc: "لیمو و نعنا", img: "images/hookah3.png" },
     { name: "آدامس نعنا", desc: "طعم آدامس و نعنا", img: "images/hookah4.png" },
